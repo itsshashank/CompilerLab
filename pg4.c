904446ea7468b8d5517428f53307dc1ee87638b0
Shashank Babu <shashank.sj1@programmer.net>
2019-10-24 06:16:32 +0000

    4

--- pg4.c
+++ pg4.c
@@ -1,171 +1,295 @@
 #include<stdio.h>
-#include<stdlib.h>
-struct node
-{
-    int q;
-    struct node *l;
-};
+int Fa[10][10][10],states[2][10],row=0,col=0,sr=0,sc=0,th=0,
+in,stat,new_state[10][10],max_inp=-1,no_stat;
+FILE *fp;
 
-static int set[20], nostate, noalpha, s, notransition, c, r, buffer[20];
-char alphabet[20];
-static int e_closure[20][20] = {0};
-struct node *transition[20][20] = {NULL};
-struct node new_transition[20][20];
-char newstates[20][20];
-// int flag[20];
+int search(int search_var)
+ {
 
-int isinput(char c)
-{
-    int i;
-    for (i = 0; i < noalpha; i++)
-        if (alphabet[i] == c)
-            return i;
-    return -1;
-}
+   int i;
+   for(i=0;i<no_stat;i++)
+      if(search_var == states[1][i])
+      return 1;
+   return 0;
+ }
 
-void insert_trasit(int r, char c, int s)
-{
-    int j;
-    struct node *temp;
-    j = isinput(c);
-    if (j == -1)
+
+int sort(int *arr,int count)
+ {
+   int temp,i,j;
+   for(i=0;i<count-1;i++)
+     {
+      for(j=i+1;j<count;j++)
     {
-        printf("error\n");
-        exit(0);
-    }
-    temp = (struct node *)malloc(sizeof(struct node));
-    temp->q = s;
-    temp->l = transition[r][j];
-    transition[r][j] = temp;
-}
-
-void insert_trasit_new(int r, char c, int s)
-{
-    int j;
-    struct node *temp;
-    j = isinput(c);
-    if (j == -1)
-    {
-        printf("error\n");
-        exit(0);
-    }
-    temp = (struct node *)malloc(sizeof(struct node));
-    temp->q = s;
-    temp->l = new_transition[r][j];
-    new_transition[r][j] = temp;
-}
-
-
-void findclosure(int x, int sta)
-{
-    struct node *temp;
-    int i;
-    if (buffer[x])
-        return;
-    e_closure[sta][c++] = x;
-    buffer[x] = 1;
-    if (alphabet[noalpha - 1] == 'e' && transition[x][noalpha - 1] != NULL)
-    {
-        temp = transition[x][noalpha - 1];
-        while (temp != NULL)
+      if(arr[i]>=arr[j])
         {
-            findclosure(temp->q, sta);
-            temp = temp->l;
-        }
-    }
-}
+          temp=arr[i];
+          arr[i]=arr[j];
+          arr[j]=temp;
+         }
+     }
+     }
+   return 0;
+ }
 
-void print_e_closure(int i)
-{
-    int j;
-    // flag[i]=0;
-    printf("{");
-    if(e_closure[i][0]!=0)
-        printf("q%d", e_closure[i][0]);
-    for (j = 1; e_closure[i][j] != 0; j++){
-        // flag[i]=1;
-        printf(",q%d", e_closure[i][j]);
-    }
-    printf("}\n");
-    // printf(" FLag : %d",flag[i]);
-}
+int checkcon(int *arr,int *count)  //for doing this {4,1}={1,2,1}=={1,2}
+ {
+   int i,temp,j,k,c,t,m;
+   for(i=0;i<*count;i++)
+     {
+       if(arr[i]>row)
+      {
+         temp =arr[i];
+         c=0;
+         t=0;
+        while(new_state[arr[i]][t]!=-1)
+         {
+           t++;
+           c++;
+         }
+        //right shift from ith postion (c-2) th time
+        for(k=0;k<=c-2;k++)
+           {
+        for(j=9;j>=i+1+k;j--)
+         {
+          arr[j]=arr[j-1];
+         }
+           }
 
-void insert2newt(){
-    //add old trasitions
-    for(int i=1;i <=nostate;i++)
-    {
-        for(int j=0;j<noalpha-1;j++){
-            new_transition[i][j]= transition[i][j];
-        }
-        int j=1;
-        while (e_closure[i][j]!=0) {
-            for(int k=0;k<noalpha-1 ;k++){
-                insert_trasit_new(i, alphabet[k],e_closure[i][j]);
-            }
-            j++;
-        }
-    }
-}
-
-void print_new(){
-    insert2newt();
-    struct node *temp;
-    printf("\n DFA ");
-    for(int i=1;i<=nostate;i++){
-        printf("\n\t Q%d",i);
-        for(int j=0;j<noalpha-1;j++){
-            printf("\n\t\t on %c :",alphabet[j]);
-            temp = new_transition[i][j];
-            if(temp!=NULL){
-                printf("Q%d",temp->q);
-                temp = temp->l;
-            }
-            else
-                printf("NULL");
-            while(temp!=NULL){
-                printf(",Q%d",temp->q);
-                temp = temp-> l;
-            }
-        }
-    }
-    printf("\n");
-}
-
-int main(int argc, char const *argv[])
-{
-    int i, j, k, m, t, n;
-    printf("Enter the number of alphabets?\n");
-    scanf("%d", &noalpha);
-    getchar();
-    printf("\nEnter alphabets? and epsilon as e at the end\n");
-    for (i = 0; i < noalpha; i++)
-    {
-        alphabet[i] = getchar();
-        getchar();
-    }
-    printf("\nEnter the number of states?\n");
-    scanf("%d", &nostate);
-    printf("\nEnter no of transition?\n");
-    scanf("%d", &notransition);
-    printf("\nEnter transition? A on 1 -> B\n");
-    for (i = 0; i < notransition; i++)
-    {
-        scanf("%d %lc %d", &r, &c, &s);
-        insert_trasit(r, c, s);
-    }
-    printf("\ne-closure of states");
-    for (i = 1; i <= nostate; i++)
-    {
-        c = 0;
-        for (j = 0; j < 20; j++)
+         t=0;
+         for(j=i;j<c;j++)
         {
-            buffer[j] = 0;
-            e_closure[i][j] = 0;
-        }
-        findclosure(i, i);
-        printf("\ne-closure(q%d): ", i);
-        print_e_closure(i);
+           arr[j]=new_state[temp][t];
+           t++;
+         }
+       }
+     }
+   c=0;
+  for(i=0;arr[i]!=-1;i++)
+      c++;
+   *count=c;
+   return 0;
+ }
+
+int remove_duplicate(int *arr,int *count)
+ {
+   int i,j=0;
+   for(i=1;i<*count;i++)
+     {
+      if(arr[i]!=arr[j])
+     {
+       j++;
+       arr[j]=arr[i];
+     }
+     }
+   *count=j+1;
+   return 0;
+ }
+
+int check(int i ,int j,int c,int *name)///for checking is this a new state?
+  {
+    int t,l,f;
+    for(l=0;l<=stat;l++)
+      {
+       t=0; f=0;
+       while(Fa[i][j][t]!=-1)
+     {
+        if(Fa[i][j][t]==new_state[l][t])
+          t++;
+        else
+          {
+        f=1;
+        break;
+         }
+     }
+      if((t==c)&&!f)
+    {
+     *name=l;
+     return 1;
     }
-    print_new();
+    }
     return 0;
 }
+
+int trans(int i ,int j,int t,int c,int *count,int *arr)//transition o/p for particular i/p on states
+ {
+   int k=0,co,temp;
+   *count=0;
+   for(k=0;k<c;k++)
+     {
+       temp=Fa[i][j][k];
+       co=0;
+       while(Fa[temp][t][co]!=-1)
+      {
+        arr[*count]=Fa[temp][t][co++];
+        (*count)++;
+      }
+     }
+return 0;
+ }
+
+int nfa2dfa(int start,int end)
+ {
+   int j,t,c,i,k,count,arr[10],name,l;
+   for(i=start;i<=end;i++)
+      {
+    for(j=0;j<=max_inp;j++)
+      {
+        c=0;t=0;
+        while(Fa[i][j][t]>=0)
+           {
+         t++;
+         c++;
+           }
+          if(c>1)
+        {
+         if(check(i,j,c,&name)==0)
+           {
+             for(k=0;k<c;k++)
+              {
+             new_state[stat][k]=Fa[i][j][k];
+             for(l=0;states[1][l]!=-1;l++)
+                if(new_state[stat][k] == states[1][l]&& !search(stat))
+                  states[1][no_stat++]=stat;
+              }
+
+              for(t=0;t<=max_inp;t++)
+            {
+               count=0;
+               for(k=0;k<10;k++)
+                  arr[k]=-1;
+               trans(i,j,t,c,&count,arr);
+
+
+               checkcon(arr,&count);
+
+               sort(arr,count);
+               remove_duplicate(arr,&count);
+
+               for(k=0;k<count;k++)
+                 Fa[stat][t][k]=arr[k];
+            }
+          Fa[i][j][0]=stat++;
+          for(t=1;t<c;t++)
+            Fa[i][j][t]=-1;
+         }
+           else
+         {
+           Fa[i][j][0]=name ;
+           for(t=1;t<c;t++)
+             Fa[i][j][t]=-1;
+         }
+          }
+       }
+
+     }
+  return 0;
+ }
+
+int main()
+ {
+
+     int i,j,k,flag=0,start,end;
+     char c,ch;
+     fp=fopen("input.txt","r+");
+
+
+     for(i=0;i<2;i++)
+       for(j=0;j<10;j++)
+     states[i][j]=-1;
+
+    for(i=0;i<10;i++)
+       for(j=0;j<10;j++)
+     new_state[i][j]=-1;
+
+     for(i=0;i<10;i++)
+       for(j=0;j<10;j++)
+      for(k=0;k<10;k++)
+          Fa[i][j][k]=-1;
+
+     while(fscanf(fp,"%d",&in)!=EOF)
+       {
+       fscanf(fp,"%c",&c);
+
+        if(flag)
+          {
+         states[sr][sc++]=in;
+         if(c=='\n')
+            {
+              sr++;
+              sc=0;
+            }
+          }
+        else if(c=='#')
+          {
+         flag=1;
+         Fa[row][col][th]=in;
+
+          }
+         else if(!flag)
+          {
+         Fa[row][col][th]=in;
+         if(c==',')
+           {
+             th++;
+           }
+         else if(c=='\n')
+           {
+             if(max_inp<col)
+               max_inp=col;
+              col=0;
+              row++;
+              th=0;
+          }
+        else if(c!=',')
+         {
+             col++;
+             th=0;
+          }
+          }
+
+      }
+   no_stat=0;
+   i=0;
+   while(states[1][i++]!=-1)
+      no_stat++;
+      stat=row+1;
+      start=0;end=row;
+      while(1)
+      {
+         nfa2dfa(start,end);
+         start=end+1;
+         end=row;
+         if(start>end)
+          break;
+      }
+
+     printf("\n\nDFA: \n\n\n");
+        printf("input");
+     for(i=0;i<=max_inp;i++)
+     printf("\t%d",i);
+     printf("\n");
+     for(i=0;i<stat;i++)
+       {
+     printf("%d-> |",i);
+       for(j=0;j<=max_inp;j++)
+      {
+        printf("%2d        ",Fa[i][j][0]);
+      }
+      printf("\n");
+       }
+printf("\n\n");
+printf("Total Number Of Stats: %d \n\n",stat);
+printf("Final States : ");
+  for(i=0;states[1][i]!=-1;i++)
+     printf("%d ",states[1][i]);
+
+printf("\n\n");
+//getch();
+    return 0;
+}
+
+
+
+
+
